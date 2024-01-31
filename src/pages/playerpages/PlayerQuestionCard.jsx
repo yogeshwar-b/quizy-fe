4dc9371c62bc39ccc339d244b5b0002e61932d42
yogeshwar-b <byogeshwarreddy@gmail.com>
2024-01-31 09:54:31 +0530

    Statehandling for the question choices in playercard.

--- src/pages/playerpages/PlayerQuestionCard.jsx
+++ src/pages/playerpages/PlayerQuestionCard.jsx
@@ -1,25 +1,15 @@
 import { notify } from '../../Components/Snackbar'
-import { useEffect, useState } from 'react'
+import {
+  useEffect,
+  useRef,
+  useState,
+  forwardRef,
+  useImperativeHandle
+} from 'react'
 import '../../styles/playerquestioncard.css'
 import socket from '../../socket/socket'
 import { useParams, useNavigate } from 'react-router-dom'
 
-/**
- * @todo- delete sample data
- */
-const sampledata = {
-  _id: '65a8a2c1f5e20aa09adaddf4',
-  questionid: '802b53c4daaf50369cf0dc0e84f34fc3',
-  questiontxt: 'My question 1',
-  choices: ['choice1', 'choice2', 'choice3'],
-  answer: 1,
-  __v: 0
-}
-
-/**
- * @todo - Add reducer to handle events on the question choices.
- * @returns
- */
 export default function PlayerQuestionCard() {
   // const questionstate = sampledata
   var { roomname } = useParams()
@@ -27,7 +17,8 @@ export default function PlayerQuestionCard() {
   socket.on('receivednextquestion', (arg) => {
     // changereceiveddata(arg + '\n' + receiveddata)
     changeQuestionState(arg)
-    changedisable(false)
+    // changedisable(false)
+    choicesref.current.resetchoicestates()
     console.log('received question', arg)
   })
   const Navigate = useNavigate()
@@ -35,7 +26,7 @@ export default function PlayerQuestionCard() {
     try {
       console.log('inside join existing room')
       socket.emit('joinroom', req, function test(resp) {
-        console.log(resp)
+        // console.log(resp)
         if (resp.msg == 'JoinSuccess') {
           notify('Room was found')
           // props.isConnected({ connected: true, roomname: req.roomname })
@@ -59,41 +50,77 @@ export default function PlayerQuestionCard() {
     })
   }, [])
 
-  const [disable, changedisable] = useState(false)
+  // const [disable, changedisable] = useState(false)
   const [questionstate, changeQuestionState] = useState({
-    _id: '65a8a2c1f5e20aa09adaddf4',
-    questionid: '802b53c4daaf50369cf0dc0e84f34fc3',
+    _id: 'xx',
+    questionid: 'xx',
     questiontxt: 'Waiting for First Quesition',
     choices: [],
-    answer: 1,
+    answer: 0,
     __v: 0
   })
-  let count = 0
+
+  const choicesref = useRef(null)
   return (
     <div>
-      <div> Player question card below</div>
+      {/* <div> Player question card below</div> */}
       <div>{questionstate.questiontxt}</div>
-      <div className='choices-list'>
-        {questionstate.choices?.map((choice) => {
-          return (
-            <button
-              disabled={disable}
-              className='choice-button'
-              id={count++}
-              key={count}
-              onClick={(e) => {
-                console.log(e.target.innerText)
-                changedisable(!disable)
-                e.target.classList.add('submitted')
-                notify('choice submitted')
-                //send e.target.id to backend
-              }}
-            >
-              {choice}
-            </button>
-          )
-        })}
-      </div>
+      <Choices choicedata={questionstate.choices} ref={choicesref} />
     </div>
   )
 }
+
+const Choices = forwardRef(function Choices(props, ref) {
+  const [ChoiceState, changeChoiceState] = useState({
+    isSubmitted: false,
+    Selection: -1
+  })
+  useImperativeHandle(ref, () => {
+    return {
+      resetchoicestates() {
+        changeChoiceState({
+          isSubmitted: false,
+          Selection: -1
+        })
+      }
+    }
+  })
+
+  let count = 0
+  return (
+    <div className='choices-list'>
+      {props.choicedata?.map((choice) => {
+        return !ChoiceState.isSubmitted ? (
+          <button
+            className='choice-button'
+            id={count++}
+            key={count}
+            onClick={(e) => {
+              // e.target.classList.add('submitted')
+              changeChoiceState({
+                isSubmitted: true,
+                Selection: e.target.innerText
+              })
+              notify('choice submitted')
+              //send e.target.id to backend
+            }}
+          >
+            {choice}
+          </button>
+        ) : (
+          <button
+            disabled
+            className={
+              'choice-button ' +
+              (ChoiceState.Selection === choice ? 'submitted' : '')
+            }
+            id={count++}
+            key={count}
+          >
+            {choice}
+          </button>
+        )
+      })}
+    </div>
+  )
+})
